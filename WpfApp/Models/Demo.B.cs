07aa4ee85e102132bd4469bd55ae148c239ec4cf
Paweł Łęczycki <pawel@msd.info.pl>
2022-11-28 09:11:02 +0000

    WPF

---
+++ WpfApp/Models/Demo.B.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    internal partial class Demo
+    {
+        public void Sth()
+        {
+            _a = 1;
+        }
+    }
+}
