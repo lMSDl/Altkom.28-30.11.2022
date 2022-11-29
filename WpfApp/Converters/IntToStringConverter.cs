313b8b7d18b61ad4624bdc1a266476ee69fdb099
Paweł Łęczycki <pawel@msd.info.pl>
2022-11-29 15:39:39 +0100

    MarkupExtension

--- WpfApp/Converters/IntToStringConverter.cs
+++ WpfApp/Converters/IntToStringConverter.cs
@@ -5,19 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace WpfApp.Converters
 {
-    public class IntToStringConverter : IValueConverter
+    public class IntToStringConverter : MarkupExtension, IValueConverter
     {
+        public int? MaxValue { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var intValue = (int)value;
+            if (MaxValue.HasValue && intValue > MaxValue)
+                return MaxValue.Value.ToString();
             return value.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return int.TryParse(value.ToString(), out var result) ? result : 0;
+            var intResult = int.TryParse(value.ToString(), out var result) ? result : 0;
+            
+            return MaxValue.HasValue && intResult > MaxValue ? MaxValue : intResult;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
         }
     }
 }
