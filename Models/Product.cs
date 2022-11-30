a6cbe5b8fc8a21a7962308236212ec2138ab561c
Paweł Łęczycki <pawel@msd.info.pl>
2022-11-30 12:36:59 +0100

    FluentValidation

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,32 +1,36 @@
-﻿using System.Collections;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Models.Validators;
+using System.Collections;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Models
 {
-    public class Product : INotifyPropertyChanged, ICloneable, IDataErrorInfo, INotifyDataErrorInfo
+    public class Product : INotifyPropertyChanged, ICloneable, /*IDataErrorInfo,*/ INotifyDataErrorInfo
     {
         private string name = "";
         private DateTime expirationDate;
         private decimal price;
-
+        private readonly ProductValidator _validator = new ProductValidator();
+        private ValidationResult _validationResult;
 
         public string Name
         {
             get => name;
             set
             {
-                if (value.Length < 5)
-                {
-                    _errorDictionary[nameof(Name)] = "Nazwa musi mieć conajmniej 5 znaków";
-                }
-                else
-                {
-                    _errorDictionary.Remove(nameof(Name));
-                }
+                //if (value.Length < 5)
+                //{
+                //    _errorDictionary[nameof(Name)] = "Nazwa musi mieć conajmniej 5 znaków";
+                //}
+                //else
+                //{
+                //    _errorDictionary.Remove(nameof(Name));
+                //}
                 name = value;
                 OnPropertyChanged();
-                OnErrorChanged();
+                //OnErrorChanged();
 
             }
         }
@@ -59,32 +63,35 @@ namespace Models
         public void OnPropertyChanged([CallerMemberName] string nameOfProperty = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameOfProperty));
+            OnErrorChanged();
         }
 
 
-        #region IDataErrorInfo
-        private Dictionary<string, string> _errorDictionary { get; } = new Dictionary<string, string>();
+        //#region IDataErrorInfo
+        //private Dictionary<string, string> _errorDictionary { get; } = new Dictionary<string, string>();
 
-        public string this[string columnName] => _errorDictionary.TryGetValue(columnName, out var error) ? error : string.Empty;
-        public string Error => string.Join("\n", _errorDictionary.Select(x => x.Value));
+        //public string this[string columnName] => _errorDictionary.TryGetValue(columnName, out var error) ? error : string.Empty;
+        //public string Error => string.Join("\n", _errorDictionary.Select(x => x.Value));
 
-        #endregion IDataErrorInfo
+        //#endregion IDataErrorInfo
 
 
-        #region IDataErrorInfo
+        #region INotifyDataErrorInfo
         public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
 
         public IEnumerable GetErrors(string? propertyName)
         {
-            return _errorDictionary.Select(x => x.Value);
+            return _validationResult.Errors.Where(x => x.PropertyName == propertyName).Select(x => x.ErrorMessage).ToList();
+            //return _errorDictionary.Select(x => x.Value);
         }
-        public bool HasErrors => _errorDictionary.Any();
+        public bool HasErrors => !_validationResult?.IsValid ?? false; //_errorDictionary.Any();
 
         public void OnErrorChanged([CallerMemberName] string nameOfProperty = "")
         {
+            _validationResult = _validator.Validate(this);
             ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameOfProperty));
         }
-        #endregion IDataErrorInfo
+        #endregion INotifyDataErrorInfo
 
     }
 }
---
+++ Models/Validators/ProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Validators
+{
+    internal class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator() { 
+            
+            RuleFor(x => x.Name).MinimumLength(5).NotEmpty().NotNull().WithName("Nazwa");
+            RuleFor(x => x.ExpirationDate).GreaterThan(DateTime.Now);
+        }
+    }
+}
