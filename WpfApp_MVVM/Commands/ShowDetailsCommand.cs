6d8e0b1d581ccc41b76457ac6196f2e6e696bd5a
Paweł Łęczycki <pawel@msd.info.pl>
2022-11-30 09:59:15 +0100

    MVVM + ICommand

---
+++ WpfApp_MVVM/Commands/ShowDetailsCommand.cs
@@ -0,0 +1,38 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp_MVVM.Commands
+{
+    public class ShowDetailsCommand : ICommand
+    {
+        private Product? product;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public Product? Product
+        {
+            get => product;
+            set
+            {
+                product = value;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return Product != null;
+        }
+
+        public void Execute(object? parameter)
+        {
+            MessageBox.Show($"Data przydatności: {Product!.ExpirationDate}");
+        }
+    }
+}
---
+++ WpfApp_MVVM/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Models;
+using Models.Fakers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp_MVVM.Commands;
+
+namespace WpfApp_MVVM.ViewModels
+{
+    public class ProductsViewModel
+    {
+        private Product? selectedProduct;
+
+        public ObservableCollection<Product> Products { get; }
+        public Product? SelectedProduct
+        {
+            get => selectedProduct;
+            set
+            {
+                selectedProduct = value;
+                //ShowDetailsCommand.Product = value;
+
+            }
+        }
+
+        public ProductsViewModel()
+        {
+
+            Products = new ObservableCollection<Product>(new ProductFaker().Generate(5));
+
+            //ShowDetailsCommand = new ShowDetailsCommand();
+            ShowDetailsCommand = new RelayCommand(
+                param => MessageBox.Show($"Data przydatności: {SelectedProduct!.ExpirationDate}"),
+                param => SelectedProduct != null);
+
+            DeleteCommand = new RelayCommand(
+                param => Products.Remove((Product)param),
+                param => param as Product != null);
+        }
+
+        //public ShowDetailsCommand ShowDetailsCommand { get; }
+        public ICommand ShowDetailsCommand { get; }
+        public ICommand DeleteCommand { get; }
+    }
+}
